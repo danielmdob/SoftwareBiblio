0b1302a77ce5587d8c6fb4c3a5bc5915dd702f23
josue.cubero@ucr.ac.cr <josue.cubero@ucr.ac.cr>
2018-03-08 14:37:59 -0600

    Sirve CSS

--- Core/settings.py
+++ Core/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'SoftwareBiblio/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -134,13 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '../templates/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/templates/'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/SoftwareBiblio/static')
 
 LOGIN_URL = 'login'
 
